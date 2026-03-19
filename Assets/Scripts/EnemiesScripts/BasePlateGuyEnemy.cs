9979bc9319602d726a3b3de8a4f366c9ecf837b5
Carlos <carloscreus@ceroca.cat>
2026-03-19 16:03:59 +0100

    Instanciar enemics funcional

--- Assets/Scripts/EnemiesScripts/BasePlateGuyEnemy.cs
+++ Assets/Scripts/EnemiesScripts/BasePlateGuyEnemy.cs
@@ -3,12 +3,22 @@ using UnityEngine;
 public class BasePlateGuyEnemy : MonoBehaviour
 {
     [SerializeField] private float vel;
+    private Camera camera;
+    Vector3 limitInferiorEsquerra;
+    //Vector3 limitSuperiorDret;
+    void Start()
+    {
+        camera = Camera.main;
+
+        limitInferiorEsquerra = camera.ViewportToWorldPoint(new Vector2(0, 0));
+        //limitSuperiorDret = camera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
 
     void Update()
     {
         transform.position += new Vector3(-vel * Time.deltaTime, 0, 0);
 
-        if (transform.position.x <= -9.58f)
+        if (transform.position.x <= limitInferiorEsquerra.x)
         {
             Destroy(gameObject);
         }
