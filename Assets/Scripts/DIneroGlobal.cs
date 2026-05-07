f4948ffe51e6a3d5392aedfda3fc124da7365194
paudonoso <paudonoso@ceroca.cat>
2026-05-07 18:32:09 +0200

    Pantalla niveles (falta el nivel 1)

--- Assets/Scripts/DIneroGlobal.cs
+++ Assets/Scripts/DIneroGlobal.cs
@@ -2,5 +2,48 @@ using UnityEngine;
 
 public class DineroGlobal : MonoBehaviour
 {
-    public int dineroGlobal = 125;
+    public static DineroGlobal Instance { get; private set; }
+
+    [SerializeField] private int defaultDinero = 125;
+    public int dineroGlobal;
+
+    private const string SaveKeyMoney = "InventoryMoney";
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        if (PlayerPrefs.HasKey(SaveKeyMoney))
+        {
+            dineroGlobal = PlayerPrefs.GetInt(SaveKeyMoney);
+        }
+        else
+        {
+            dineroGlobal = defaultDinero;
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveMoney();
+    }
+
+    private void OnDisable()
+    {
+        // In editor, stopping Play Mode can skip or reorder lifecycle calls.
+        SaveMoney();
+    }
+
+    public void SaveMoney()
+    {
+        PlayerPrefs.SetInt(SaveKeyMoney, dineroGlobal);
+        PlayerPrefs.Save();
+    }
 }
